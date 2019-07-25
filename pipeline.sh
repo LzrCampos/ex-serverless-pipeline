3074f8f2966c64f9523be42bc83b48251b16bcfc
lazaro.fonseca <lazaro.fonseca@ebix.com>
2019-07-25 14:34:02 -0300

    alter pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -6,6 +6,7 @@ bx login --apikey $DEPLOYER_API_KEY -a api.ng.bluemix.net -o "$ORG" -s "$SPACE"
 ibmcloud wsk property get --auth
 echo ">>> Install serverless"
 npm install
+npm install serverless
 echo ">> serverless version"
 npx serverless -v
 echo ">>> Deploy function"
