dfa2d6a87d698e4ef71728694e9aa4ce33fd6ec0
lazaro.fonseca <lazaro.fonseca@ebix.com>
2019-07-25 14:27:04 -0300

    save pipeline from ibm toolchain

---
+++ pipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo ">>> Checking installed version of IBM Cloud CLI…"
+bx --version
+echo ">>> Logging into IBM Cloud…"
+bx login --apikey $DEPLOYER_API_KEY -a api.ng.bluemix.net -o "$ORG" -s "$SPACE"
+ibmcloud wsk property get --auth
+echo ">>> Install serverless"
+npm install
+echo ">> serverless version"
+npx serverless -v
+echo ">>> Deploy function"
+npx serverless deploy
